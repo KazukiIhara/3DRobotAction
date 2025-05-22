06882f191cc74f6c5949cf23181a8602165f3d36
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-05-22 11:39:27 +0900

    プレイシーンを作成

---
+++ Project/App/Scenes/PlayScene.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <memory>
+#include <array>
+
+#include "Framework/MAGI.h"
+#include "MAGIUitility/MAGIUtility.h"
+
+using namespace MAGIUtility;
+
+/// <summary>
+/// ゲームプレイシーン
+/// </summary>
+/// <typeparam name="Data"></typeparam>
+template <typename Data>
+class PlayScene :public BaseScene<Data> {
+public:
+	using BaseScene<Data>::BaseScene; // 親クラスのコンストラクタをそのまま継承
+	~PlayScene()override = default;
+
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Finalize() override;
+
+private:
+	// カメラ
+	std::unique_ptr<Camera3D> sceneCamera_ = nullptr;
+	std::unique_ptr<Camera2D> sceneCamera2D_ = nullptr;
+
+	// DirectionalLight
+	DirectionalLight directionalLight_{};
+
+
+};
+
+template<typename Data>
+inline void PlayScene<Data>::Initialize() {
+
+
+}
+
+template<typename Data>
+inline void PlayScene<Data>::Update() {
+
+
+}
+
+template<typename Data>
+inline void PlayScene<Data>::Draw() {
+
+
+}
+
+template<typename Data>
+inline void PlayScene<Data>::Finalize() {
+
+
+}
