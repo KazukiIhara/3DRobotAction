98478d30aa77ff704ee8923aa37aaddcff574ed3
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-05-28 14:11:22 +0900

    最低限必要なクラスを実装

---
+++ Project/App/GameObject/AC/AC.cpp
@@ -0,0 +1,16 @@
+#include "AC.h"
+
+AC::AC() {
+}
+
+AC::~AC() {
+}
+
+void AC::Initilaize() {
+}
+
+void AC::Update() {
+}
+
+void AC::Draw() {
+}
---
+++ Project/App/GameObject/AC/AC.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/// <summary>
+/// ACクラス
+/// </summary>
+class AC {
+public:
+	AC();
+	~AC();
+
+	void Initilaize();
+	void Update();
+	void Draw();
+
+private:
+
+
+};
