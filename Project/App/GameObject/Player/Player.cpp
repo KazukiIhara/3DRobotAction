7f5e4cde7ef0cb5efa58089c2fa0028cc4bde657
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-06-12 12:31:07 +0900

    Playerが動くようにしました

--- Project/App/GameObject/Player/Player.cpp
+++ Project/App/GameObject/Player/Player.cpp
@@ -4,12 +4,28 @@
 
 Player::Player() {
 	std::shared_ptr<ModelRenderer> playerModel = std::make_shared<ModelRenderer>("Player", "teapot");
+	playerModel->GetTransform()->SetTranslate(Vector3(0.0f, 1.0f, 0.0f));
+
 	std::shared_ptr<GameObject3D> obj = std::make_shared<GameObject3D>("Player");
 	obj->AddModelRenderer(std::move(playerModel));
 	playerObject_ = MAGISYSTEM::AddGameObject3D(std::move(obj));
 }
 
 void Player::Update() {
+
+	Vector2 stick{};
+	if (MAGISYSTEM::IsPadConnected(0)) {
+		stick.x = MAGISYSTEM::GetLeftStickX(0);
+		stick.y = MAGISYSTEM::GetLeftStickY(0);
+	}
+
+	velocity_ = Normalize(Vector3(stick.x, 0.0f, stick.y));
+	velocity_ *= speed_ * MAGISYSTEM::GetDeltaTime();
+
+	if (auto it = playerObject_.lock()) {
+		it->GetTransform()->AddTranslate(velocity_);
+	}
+
 	// 破壊時エフェクトテスト
 	if (ImGui::Button("PlayEffect")) {
 		breakEffect_ = std::make_unique<BreakEffect>(Vector3(0.0f, 0.0f, 0.0f));
--- Project/App/GameObject/Player/Player.h
+++ Project/App/GameObject/Player/Player.h
@@ -26,6 +26,9 @@ private:
 	// 破壊時のエフェクト
 	std::unique_ptr<BreakEffect> breakEffect_ = nullptr;
 
+	Vector3 velocity_;
+	float speed_ = 2.0f;
+
 	// デバッグ用変数
 	bool isBreak_ = false;
 	Vector4 color_ = Color::White;
