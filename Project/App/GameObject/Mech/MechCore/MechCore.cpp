11f31f6b71ae0fa0f395c1fa10bcac291051cec3
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-06-18 17:01:43 +0900

    実装の方向性決定。 MachCoreとMechLegを実装して、基本動作ができるところまで実装する。

--- Project/App/GameObject/Mech/MechCore/MechCore.cpp
+++ Project/App/GameObject/Mech/MechCore/MechCore.cpp
@@ -19,13 +19,32 @@ MechCore::MechCore() {
 }
 
 void MechCore::Update() {
-	if (auto cs = currentState_.lock()) {
+	if (auto cs = currentState_.second.lock()) {
 		cs->Update(this);
 	}
 }
 
 void MechCore::ChangeState(MechCoreState nextState) {
+	if (auto cs = currentState_.second.lock()) {
+		cs->Exit(this);
+	}
+	currentState_ = std::make_pair(nextState, GetState(nextState));
 
+	if (auto cs = currentState_.second.lock()) {
+		cs->Enter(this);
+	}
+}
+
+const MechCoreState& MechCore::GetCurrentState() const {
+	return currentState_.first;
+}
+
+const Vector3& MechCore::GetVelocity()const {
+	return velocity_;
+}
+
+void MechCore::SetVelocity(const Vector3& velocity) {
+	velocity_ = velocity;
 }
 
 std::weak_ptr<MechCoreBaseState> MechCore::GetState(MechCoreState state) {
--- Project/App/GameObject/Mech/MechCore/MechCore.h
+++ Project/App/GameObject/Mech/MechCore/MechCore.h
@@ -4,15 +4,30 @@
 #include <unordered_map>
 
 // MyHedder
+#include "Math/Utility/MathUtility.h"
+
 #include "MechCoreStates/MechCoreBaseState.h"
+
+// 部位ごとのクラス
+#include "GameObject/Mech/MechLeg/MechLeg.h"
+
 
 // 前方宣言
 class GameObject3D;
 
+// 入力コマンド
+struct InputCommand {
+	Vector2 direction = { 0.0f,0.0f };
+	bool jump = false;
+	bool quickBoost = false;
+	bool assultBoost = false;
+};
+
 // 状態
 enum class MechCoreState {
 	Root,
-	
+	QuickBoost,
+	AsslutBoost
 };
 
 /// <summary>
@@ -26,6 +41,11 @@ public:
 	void Update();
 	void ChangeState(MechCoreState nextState);
 
+	const MechCoreState& GetCurrentState()const;
+	const Vector3& GetVelocity()const;
+
+	void SetVelocity(const Vector3& velocity);
+
 private:
 	std::weak_ptr<MechCoreBaseState> GetState(MechCoreState state);
 private:
@@ -34,5 +54,12 @@ private:
 	// ステートテーブル
 	std::unordered_map<MechCoreState, std::shared_ptr<MechCoreBaseState>> states_;
 	// 現在のステート
-	std::weak_ptr<MechCoreBaseState> currentState_;
+	std::pair<MechCoreState, std::weak_ptr<MechCoreBaseState>> currentState_;
+	
+
+
+private:
+	// パラメータ
+	Vector3 velocity_ = { 0.0f,0.0f,0.0f };
+	bool onGround_ = false;
 };
