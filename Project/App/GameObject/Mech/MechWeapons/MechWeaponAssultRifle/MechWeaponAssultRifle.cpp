a27c3f4da7f29a0c49f8e5a955b3bd3a415b6926
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-07-14 14:04:02 +0900

    プレイヤーUIクラスを実装

--- Project/App/GameObject/Mech/MechWeapons/MechWeaponAssultRifle/MechWeaponAssultRifle.cpp
+++ Project/App/GameObject/Mech/MechWeapons/MechWeaponAssultRifle/MechWeaponAssultRifle.cpp
@@ -1,8 +1,13 @@
 #include "MechWeaponAssultRifle.h"
 
+#include "3D/GameObject3D/GameObject3D.h"
+#include "3D/Transform3D/Transform3D.h"
+
 MechWeaponAssultRifle::MechWeaponAssultRifle()
 	:BaseMechWeapon("AssultRifle", WeaponType::AssultRifle) {
-	
+	if (auto obj = weapon_.lock()) {
+		obj->GetTransform()->SetTranslateZ(0.6f);
+	}
 }
 
 void MechWeaponAssultRifle::Update(MechCore* mechCore) {
