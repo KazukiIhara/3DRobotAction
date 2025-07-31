75a95f3b199497e445c088fb034b8f78bc492a6b
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-07-31 10:16:07 +0900

    必要なファイルを追加

--- Project/Engine/Includes/Common/ParticleEffectCommon.h
+++ Project/Engine/Includes/Common/ParticleEffectCommon.h
@@ -36,39 +36,10 @@ struct EmitParticleEffectDataForGPU {
 	Vector3 velocity;
 	float pad3;
 
-	bool isUseBillboard;
-	Vector3 pad4;
-
-	float lifeTime;
-	Vector3 pad5;
-
-	float currentTime;
-	Vector3 pad6;
-};
-
-
-/// <summary>
-/// パーティクルエフェクトのGPUに送る更新用データ
-/// </summary>
-struct UpdateParticleEffectDataForGPU {
-	Vector3 scale;
-	float pad0;
-
-	Vector3 rotate;
-	float pad1;
-
-	Vector3 translate;
-	float pad2;
-
-	Vector3 velocity;
-	float pad3;
-
-	Vector4 color;
-
 	uint32_t texIndex;
 	Vector3 pad4;
 
-	bool isUseBillboard;
+	uint32_t isUseBillboard;
 	Vector3 pad5;
 
 	float lifeTime;
