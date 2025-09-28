f841b3ff9a91952c575206b56054fa3907ab7a04
KazukiIhara <iharakazuki0829@gmail.com>
2025-09-29 08:24:42 +0900

    ミサイルに必要なクラスを作成

---
+++ Project/App/GameObject/Missile/Missile.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// C++
+#include <memory>
+
+// MyHedder
+#include "Math/Utility/MathUtility.h"
+#include "GameObject/AttackCollider/AttackCollider.h"
+
+// 前方宣言
+class GameObject3D;
+
+// ミサイルの種類
+enum MissileType {
+	Dual,
+};
+
+
+
+/// <summary>
+/// ミサイル
+/// </summary>
+class Missile {
+public:
+	Missile();
+	~Missile() = default;
+
+private:
+	// ゲームオブジェクト
+	std::weak_ptr<GameObject3D> missile_;
+
+};
