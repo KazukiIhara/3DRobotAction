a48dd0ffb89d8462c0a31f89f8e17588c583f9f6
KazukiIhara <iharakazuki0829@gmail.com>
2025-10-19 00:56:31 +0900

    発射位置を調整できるようになりました

--- Project/App/GameObject/MechWeapon/Hand/MechHandWeapon.cpp
+++ Project/App/GameObject/MechWeapon/Hand/MechHandWeapon.cpp
@@ -4,17 +4,40 @@
 #include "MAGI.h"
 
 MechHandWeapon::MechHandWeapon(const MechHandWeapon::Param& param) {
-
-}
-
-void MechHandWeapon::Initialize() {
-
+	// トランスフォーム作成
+	std::unique_ptr<Transform3D> transform = std::make_unique<Transform3D>();
+	// マネージャに追加
+	transform_ = MAGISYSTEM::AddTransform3D(std::move(transform));
+	param_ = param;
 }
 
 void MechHandWeapon::Update() {
-
+	// 弾発射位置を計算
+	CulFirePos();
 }
 
 void MechHandWeapon::Draw() {
+	// 描画
+	MAGISYSTEM::DrawModel(param_.modelName, transform_->GetWorldMatrix(), material_);
+}
 
+void MechHandWeapon::Attack() {
+	
+}
+
+MechHandWeapon::Param& MechHandWeapon::GetParam() {
+	return param_;
+}
+
+Transform3D* MechHandWeapon::GetTransform() {
+	return transform_;
+}
+
+void MechHandWeapon::CulFirePos() {
+	// ローカル行列を計算
+	param_.fireOffsetLocalMatrix = MakeTranslateMatrix(param_.fireOffsetLocalPos);	
+	// ワールド行列を計算
+	param_.fireOffsetWorldMatrix = param_.fireOffsetLocalMatrix * transform_->GetWorldMatrix();
+	// ワールド座標を計算
+	param_.fireOffsetWorldPos = ExtractionWorldPos(param_.fireOffsetWorldMatrix);
 }
