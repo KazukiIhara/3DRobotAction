46efb65610acde2ce31501925a809f1afc269927
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-10-21 13:34:37 +0900

    攻撃処理を差し替え

--- Project/App/GameObject/Mech/MechComponents/Attack/MechAttackComponent.h
+++ Project/App/GameObject/Mech/MechComponents/Attack/MechAttackComponent.h
@@ -14,7 +14,6 @@ public:
 	void Update(MechCore* mechCore);
 
 	// 攻撃
-	void AttackLeftHand(MechCore* mechCore);
 	void AttackRightHand(MechCore* mechCore);
 	void AttackLeftShoulder(MechCore* mechCore);
 	void AttackRightShoulder(MechCore* mechCore);
