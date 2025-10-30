a57e890dff3e1f06301f4f09eea188f2f1591319
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-10-30 15:02:39 +0900

    ロケットクラスを実装

--- Project/App/GameObject/AttackObjects/Rocket/Rocket.h
+++ Project/App/GameObject/AttackObjects/Rocket/Rocket.h
@@ -1,1 +1,52 @@
 #pragma once
+
+// C++
+#include <memory>
+
+// MyHedder
+#include "Math/Utility/MathUtility.h"
+#include "GameObject/AttackCollider/AttackCollider.h"
+
+// 前方宣言
+class GameObject3D;
+
+/// <summary>
+/// ロケット弾
+/// </summary>
+class Rocket {
+public:
+	Rocket(const Vector3& dir,
+		float speed,
+		const Vector3& wPos,
+		std::weak_ptr<AttackCollider> attackCollider);
+
+	~Rocket() = default;
+
+	void Update();
+	void Draw();
+
+	void Finalize();
+
+	bool GetIsAlive()const;
+
+	AttackCollider* GetAttackCollider();
+
+	Vector3 GetWorldPos();
+
+private:
+	// ゲームオブジェクト
+	std::weak_ptr<GameObject3D> rocket_;
+
+	// 弾の向き
+	Vector3 dir_ = { 0.0f,0.0f,0.0f };
+	// 弾の速度
+	float speed_ = 0.0f;
+
+	// 最大生存時間
+	float lifeTime_ = 5.0f;
+	// 生存フラグ
+	bool isAlive_ = false;
+
+	// 攻撃コライダー
+	std::weak_ptr<AttackCollider> collider_;
+};
