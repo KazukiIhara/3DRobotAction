57001e32cea51836f0c75acd836278157b93c7ec
KazukiIhara <iharakazuki0829@gmail.com>
2025-11-30 21:14:29 +0900

    敵AIの意思決定パラメータ構造体を追加

--- Project/App/GameObject/EnemyAI/EnemyAI.h
+++ Project/App/GameObject/EnemyAI/EnemyAI.h
@@ -26,6 +26,7 @@ enum class RootDir {
 	Right,
 };
 
+// 回避用コライダー
 struct AvoidCollider {
 	// 胴体のワールド座標
 	Vector3 wPos;
@@ -41,9 +42,14 @@ struct AvoidCollider {
 	float fovY = 65.0f;
 };
 
-// AIの
-struct AIDecisionParam {
-
+// AIの行動決定パラメータ
+struct EnemyAIDecisionParam {
+	// 集中力(次の行動までの間隔が早くなる)
+	float focus = 50.0f;
+	// 合理性(ミスプレイが増える)
+	float logic = 50.0f;
+	// 積極性(攻撃的な行動が増える)
+	float aggressive = 50.0f;
 };
 
 // 
