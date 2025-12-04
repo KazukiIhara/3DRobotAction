c5b8bf93f1370d3507a1e3596a333db8461f4a2a
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-12-04 11:12:30 +0900

    機体のカスタムシーン追加

---
+++ Project/App/Scenes/MechCustomScene/MechCustomScene.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// フレームワーク
+#include "MAGI.h"
+
+/// <summary>
+/// 機体カスタマイズシーン
+/// </summary>
+class MechCustomScene :public BaseScene {
+public:
+	using BaseScene::BaseScene; // 親クラスのコンストラクタをそのまま継承
+	~MechCustomScene() = default;
+
+	void Initialize()override;
+	void Update()override;
+	void Draw()override;
+	void Finalize()override;
+
+private:
+
+};
