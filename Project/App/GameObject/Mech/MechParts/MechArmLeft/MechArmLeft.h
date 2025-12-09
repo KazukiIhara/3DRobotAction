beccc0d1faa9f38e9ec3f5807e7cc99915a194aa
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-12-09 15:22:20 +0900

    機体の各パーツにパラメータを追加

--- Project/App/GameObject/Mech/MechParts/MechArmLeft/MechArmLeft.h
+++ Project/App/GameObject/Mech/MechParts/MechArmLeft/MechArmLeft.h
@@ -16,6 +16,32 @@ class MechCore;
 /// </summary>
 class MechArmLeft {
 public:
+	// パラメータ
+	struct Param {
+		// 
+		// ゲーム内で表示されるパラメータ
+		// 
+
+		// パーツの名前
+		std::string name = "ArmL";
+
+		// 体力
+		uint32_t ap = 2000;
+
+		// 
+		// 内部パラメータ
+		// 
+
+		// モデル名
+		std::string modelName = "ArmL";
+
+		// オフセット
+		Vector3 offSetPos = { 0.0f,0.0f,0.0f };
+
+		// モデルサイズ
+		Vector3 modelSize = { 0.0f,0.0f,0.0f };
+	};
+public:
 	MechArmLeft();
 	~MechArmLeft() = default;
 
--- Project/App/GameObject/Mech/MechParts/MechHead/MechHead.h
+++ Project/App/GameObject/Mech/MechParts/MechHead/MechHead.h
@@ -15,6 +15,32 @@ class MechCore;
 /// </summary>
 class MechHead {
 public:
+	// パラメータ
+	struct Param {
+		// 
+		// ゲーム内で表示されるパラメータ
+		// 
+
+		// パーツの名前
+		std::string name = "Head";
+
+		// 体力
+		uint32_t ap = 2000;
+
+		// 
+		// 内部パラメータ
+		// 
+
+		// モデル名
+		std::string modelName = "Head";
+
+		// オフセット
+		Vector3 offSetPos = { 0.0f,0.0f,0.0f };
+
+		// モデルサイズ
+		Vector3 modelSize = { 0.0f,0.0f,0.0f };
+	};
+public:
 	MechHead();
 	~MechHead() = default;
 
