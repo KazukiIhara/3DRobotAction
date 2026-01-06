d5f049cf11b010d87128f24dd23aac24b865a8c6
KazukiIhara <k023g0076@g.neec.ac.jp>
2026-01-06 11:58:56 +0900

    UIクラス実装

---
+++ Project/App/GameObject/GameUI2D/GameUI2D.cpp
@@ -0,0 +1,5 @@
+#include "GameUI2D.h"
+
+Vector2 GameUI2D::TransPosition(const Vector2& anchor) {
+    return Vector2();
+}
--- Project/App/GameObject/GameUI2D/GameUI2D.h
+++ Project/App/GameObject/GameUI2D/GameUI2D.h
@@ -1,1 +1,23 @@
 #pragma once
+
+// 数学タイプヘッダ
+#include "Math/Types/AllMathTypes.h"
+
+/// <summary>
+/// 2DゲームUI
+/// </summary>
+class GameUI2D {
+public:
+	virtual ~GameUI2D() {};
+
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+
+protected:
+	// ポジションからUIごとのアンカーポイントへ座標を変換する
+	Vector2 TransPosition(const Vector2& anchor);
+protected:
+	// UI中央座標
+	Vector2 centerPos_{};
+};
