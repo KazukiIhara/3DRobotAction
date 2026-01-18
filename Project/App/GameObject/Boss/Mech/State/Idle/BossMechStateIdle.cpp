babc3f5c15ea6b7752d9a9a0a9e19a478d68cad1
KazukiIhara <iharakazuki0829@gmail.com>
2026-01-19 08:44:22 +0900

    一旦アニメーション削除

--- Project/App/GameObject/Boss/Mech/State/Idle/BossMechStateIdle.cpp
+++ Project/App/GameObject/Boss/Mech/State/Idle/BossMechStateIdle.cpp
@@ -9,17 +9,10 @@
 using namespace Magi;
 
 void BossMechStateIdle::Enter([[maybe_unused]] BossMech* mech) {
-	idleAnimationT_ = 0.0f;
+
 }
 
 void BossMechStateIdle::Update([[maybe_unused]] BossMech* mech) {
-	mech->GetAnimator()->ApplyAnimation("BossIdle", idleAnimationT_, 0.1f);
-	idleAnimationT_ += MAGISYSTEM::GetDeltaTime() * (1.0f / 1.5f);
-
-	idleAnimationT_ = std::min(1.0f, idleAnimationT_);
-	if (idleAnimationT_ == 1.0f) {
-		idleAnimationT_ = 0.0f;
-	}
 
 }
 
