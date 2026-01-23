88534adb88145381af70233e2186832e598b4a44
KazukiIhara <iharakazuki0829@gmail.com>
2026-01-23 09:15:18 +0900

    発射体の基底クラス実装完了

--- Project/App/Feature/Damage/Object/ProjectileAttack/Base/BaseProjectileAttack.h
+++ Project/App/Feature/Damage/Object/ProjectileAttack/Base/BaseProjectileAttack.h
@@ -11,18 +11,27 @@ public:
 	struct InitParam {
 		Vector3 emitPos{};
 		Vector3 dir{};
+		float thickness = 0.0f;
+		float length = 0.0f;
 		float speed = 0.0f;
 		float life = 0.0f;
 		FriendlyTag tag;
 	};
 public:
-	BaseProjectileAttack();
+	BaseProjectileAttack(
+		const BaseProjectileAttack::InitParam& initParam,
+		DamageCollisionSystem* damageCollisionSys
+	);
 	~BaseProjectileAttack() = default;
 
+	virtual void Update()override;
+	virtual void Draw()override;
+	virtual void Finalize()override;
+
 private:
-
+	Vector3 dir_;
+	float thickness_;
+	float length_;
+	float speed_;
+	float life_;
 };
-
-BaseProjectileAttack::BaseProjectileAttack() {}
-
-BaseProjectileAttack::~BaseProjectileAttack() {}
