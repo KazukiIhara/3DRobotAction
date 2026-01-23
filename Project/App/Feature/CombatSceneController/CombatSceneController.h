6859133ef15f2b98507888757709af0865584d55
KazukiIhara <iharakazuki0829@gmail.com>
2026-01-23 16:47:45 +0900

    戦闘シーンコントロールクラスを作り始めます

---
+++ Project/App/Feature/CombatSceneController/CombatSceneController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/// <summary>
+/// 戦闘シーンを管理、コントロールするクラス
+/// </summary>
+class CombatSceneController {
+public:
+	CombatSceneController();
+	~CombatSceneController() = default;
+
+private:
+
+};
